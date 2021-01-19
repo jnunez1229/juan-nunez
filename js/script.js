39dead32aa0ef630b19f6d747ac84b3d1706d9ba
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-01-19 01:20:41 -0600

    new hero section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+VANTA.NET({
+    el: "#vanta-canvas",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3fff6c,
+    // 3fff6c
+    backgroundColor: 0x474b4f,
+  })
 
 $(function (){
     $(document).scroll(function(){
